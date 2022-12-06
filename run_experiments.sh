57eb9c33dd8afdc5023be2bbc633a449edd06110
bo40 <bo40.git@gmail.com>
2022-12-06 09:59:55 +0900

    experiment script

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -u
 
-for i in `seq 1 5`
+for i in `seq 1 10`
 do
   mkdir results
   touch results/results.log
-  python3 main.py &> results/results.log
-  mv "results" "results-$i"
+  python3 main.py --model-name first_grid --prop-name first_grid --output-dir results --min-rounds 10 --max-rounds 40 --save-files-for-each-round --target-unambiguity 0.9 --debug &> results/results.log
+  mv "results" "results-grid10-$i"
 done
