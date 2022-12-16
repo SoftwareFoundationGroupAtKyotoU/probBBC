0f5ed711df9d6bd3c270f883936d09e14d9ed763
bo40 <bo40.git@gmail.com>
2022-12-16 12:57:08 +0900

    optimize StrategyBridge.next_action()

--- StrategyBridge.py
+++ StrategyBridge.py
@@ -1,5 +1,6 @@
 import re
 import random
+import numpy as np
 from typing import Dict, Tuple, Set, List
 
 # Stateは多分本当はint
@@ -12,44 +13,53 @@ Observation = str # モデルの出力ラベルの集合
 class StrategyBridge:
 
     def __init__(self, strategy_path, states_path, trans_path, labels_path):
-        self.initial_state = 0
+        self.initial_state : int = 0
+        self.states = np.array([]) # 状態の集合 NDArray[int32]
+        self.num_states = 0 # 状態数
         # 状態sにいる確率が current_state[s]
-        self.current_state: Dict[State, float] = dict()
-        self.strategy: Dict[State, Action] = dict()# adv.traから得られたもの
+        # self.current_state: Dict[State, float] = dict()
+        self.current_state = np.array([], dtype=np.float64) # State -> Probabilityのmap. NDArray[float64]
+        # self.strategy: Dict[State, Action] = dict()# adv.traから得られたもの
         self.observation_map: Dict[State, Observation] = dict() # .prismから得られたもの
+        self.action_map: List[Action] = list()
+        self.strategy = np.array([], dtype=np.int32) # adv.traから得られたもの. State -> Action indexのmap. NDArray[int32]
         # 本当はこんなに複雑じゃなくて良いかも
         self.next_state: Dict[Tuple[State, Action, Observation], Dict[State, float]] = dict() # adv.traから得られたもの
+        self.next_state = np.array([]) # action -> transition_matrix
         self.history : List[Tuple[Action, Observation]] = []
 
-        self.__init_state_and_observation(labels_path)
-        self.__init_strategy(strategy_path)
-        states : Set[int] = set(self.strategy.keys())
-        self.current_state = dict.fromkeys(states, 0.0)
-        self.current_state[self.initial_state] = 1
-        self.states = set(self.strategy.keys())
-        self.actions = set(self.strategy.values())
-        self.empty_state : Dict[State, float] = dict.fromkeys(self.states, 0.0)
-        self.empty_dist : Dict[Action, float] = dict.fromkeys(self.actions, 0.0)
-        self.uniform_dist = [1 / len(self.actions)] * len(self.actions)
+        self.__init_state_and_observation(states_path, labels_path) # states, num_states, initial_state, observation_mapの初期化
+        self.__init_strategy(strategy_path) # strategy, next_stateの初期化
+
+        for i in range(self.num_states):
+            row_matrix = np.zeros(len(self.action_map))
+            row_matrix[self.strategy[i]] = 1.0
+            if i == 0:
+                self.strategy_matrix = np.array([row_matrix])
+            else:
+                self.strategy_matrix = np.concatenate([self.strategy_matrix, [row_matrix]])
+        self.current_state = np.zeros(self.num_states)
+        self.current_state[self.initial_state] = 1.0
+        self.empty_state = np.zeros(self.num_states)
+        self.empty_dist = np.zeros(len(self.action_map))
+        self.uniform_dist = np.full(len(self.action_map), 1.0 / len(self.action_map))
 
     def next_action(self) -> Action:
-        dist : Dict[Action, float] = self.empty_dist.copy()
-        for state, weight in self.current_state.items():
-            # self.strategy[state] : strategyでstateに対応づけられているアクション
-            if weight > 0:
-                dist[self.strategy[state]] += weight
+        # dist = self.empty_dist.copy()
+        dist = np.dot(self.current_state, self.strategy_matrix)
+        # for state, weight in self.current_state.items():
+        #     # self.strategy[state] : strategyでstateに対応づけられているアクション
+        #     if weight > 0:
+        #         dist[self.strategy[state]] += weight
         # actionがsampleされる確率がdist[action]というサンプリング
-        actions = list(dist.keys())
-        prob_dist = list(dist.values())
 
         # TODO: 分岐処理は、current_stateが全て0になる場合をupdate_stateやMultiVestaで適切に処理すれば不要
-        action : Action = ""
-        if sum(prob_dist) == 0.0:
-            action = random.choices(actions, self.uniform_dist, k=1)[0]
+        if dist.sum() == 0.0:
+            action_index = random.choices(range(len(self.action_map)), self.uniform_dist, k=1)[0]
         else:
-            action = random.choices(actions, prob_dist, k=1)[0]
+            action_index = random.choices(range(len(self.action_map)), dist, k=1)[0]
 
-        return action
+        return self.action_map[action_index]
 
     def update_state(self, action: Action, observation: Observation) -> bool:
         # 肝: actionはStrategy.next_actionで得られたものだが、observationはblack-boxなMDPを動かして観測されたもの
@@ -58,18 +68,20 @@ class StrategyBridge:
         # 2. それをaalpyにequivalence queryの反例として返す (これが反例になっているのは、今のobservationの発生確率が0 vs. 非ゼロなのでそう)
         # (ということはreplay用に入出力の列を覚えておかないといけない)
         self.history.append((action, observation))
-        new_state: Dict[State, float] = self.empty_state.copy()
+        new_state = self.empty_state.copy()
         observation = StrategyBridge.__sort_observation(observation)
 
-        for state, weight in self.current_state.items():
+        # for state, weight in self.current_state.items():
+        for state in range(self.num_states):
+            weight = self.current_state[state]
             # new_state += weight * self.next_state[state, action, observation]
             if weight > 0:
                 if (state, action, observation) in self.next_state:
                     dist = self.next_state[(state, action, observation)]
                     for s, prob in dist.items():
                         # これはadv.traのバグだが、状態がadv.traに書いていないのでどうしようもない
-                        if prob > 0 and s in new_state:
-                            new_state[s] = new_state[s] + (weight * prob)
+                        # if prob > 0 and s in new_state:
+                        new_state[s] = new_state[s] + (weight * prob)
                 else:
                     # TODO: found counterexample?
                     # if weight > 0:
@@ -77,31 +89,37 @@ class StrategyBridge:
                     pass
         # new_stateの正規化
         # regularized_new_state : Dict[State, float] = dict()
-        prob_sum = sum(new_state.values())
+        prob_sum = new_state.sum()
         if prob_sum == 0.0:
             # observationに対応する次の状態が存在しない
             self.current_state = new_state
             return False
-
-        for s in new_state:
-            new_state[s] = new_state[s] / prob_sum
-
+        new_state = new_state / prob_sum
         self.current_state = new_state
         return True
 
     # strategyをresetするためのmethod
     def reset(self):
-        states : Set[int] = set(self.strategy.keys())
-        self.current_state = dict.fromkeys(states, 0.0)
+        self.current_state = np.zeros(self.num_states)
         self.current_state[self.initial_state] = 1
         self.history = []
 
     # PRISMのモデル記述ファイルから初期状態を読み込む
-    def __init_state_and_observation(self, labels_path):
+    def __init_state_and_observation(self, states_path, labels_path):
+        state_regex = re.compile(r"(\d+):.*")
         init_regex = re.compile(r".*?(\d+)=\"init\".*")
         label_name_regex = re.compile(r"(\d+)=\"(\w+)\"")
         label_regex = re.compile(r"(\d+):(.*)")
         label_index_regex = re.compile(r"(\d+)")
+        max_state_num = 0
+        with open(states_path) as f:
+            for line in f:
+                state_match = state_regex.match(line)
+                if state_match:
+                    if int(state_match[1]) > max_state_num:
+                        max_state_num = int(state_match[1])
+        self.num_states = max_state_num + 1
+        self.states = np.arange(self.num_states, dtype=np.int32)
         labels_name = dict()
         with open(labels_path) as f:
             for line in f:
@@ -131,34 +149,12 @@ class StrategyBridge:
         for k in self.observation_map.keys():
             self.observation_map[k] = StrategyBridge.__sort_observation(self.observation_map[k])
 
-    def __init_state_and_observation2(self, prism_model_path):
-        init_regex = re.compile(r"loc : \[\d+\.\.\d+\] init (\d+);.*")
-        label_regex = re.compile(r"label \"(\w+)\" = ([\w=|]+);.*")
-        loc_regex = re.compile(r"loc=(\w+)")
-        with open(prism_model_path) as f:
-            for line in f:
-                init_match = init_regex.match(line)
-                if init_match:
-                    # 初期状態の読み込み
-                    self.initial_state = int(init_match[1])
-                else:
-                    label_match = label_regex.match(line)
-                    if label_match:
-                        # labelを読み込んでobservationsに保存する
-                        states = loc_regex.findall(label_match[2])
-                        for state in states:
-                            if int(state) in self.observation_map:
-                                self.observation_map[int(state)] = self.observation_map[int(state)] + "__" + label_match[1]
-                            else:
-                                self.observation_map[int(state)] = label_match[1]
-        for k in self.observation_map.keys():
-            self.observation_map[k] = StrategyBridge.__sort_observation(self.observation_map[k])
-
 
     # PRISMの反例ファイルを読み込んで strategyとnext_stateを初期化する
     def __init_strategy(self, strategy_path):
         regex = re.compile(r"(\d+) \d\.?\d* (\d+) (\d\.?\d*) (\w+)")
         next_state_temp : Dict[Tuple[State, Action, Observation], Dict[State, float]] = dict()
+        self.strategy = np.zeros(self.num_states, dtype=np.int32)
         with open(strategy_path) as f:
             for line in f:
                 match = regex.match(line)
@@ -167,7 +163,10 @@ class StrategyBridge:
                     next_s = int(match[2])
                     prob = float(match[3])
                     action = match[4]
-                    self.strategy[current_s] = action
+                    if not action in self.action_map:
+                        self.action_map.append(action)
+                    action_index = self.action_map.index(action)
+                    self.strategy[current_s] = action_index
                     obsv = ""
                     if next_s in self.observation_map:
                         obsv = self.observation_map[next_s]
@@ -180,6 +179,7 @@ class StrategyBridge:
                     # print("match error(__init_strategy):" + line)
                     pass
         # next_stateの要素がdistributionになるように正規化する必要がある
+
         self.next_state = dict()
         for k, prob_map in next_state_temp.items():
             prob_sum = sum(prob_map.values())
@@ -189,6 +189,6 @@ class StrategyBridge:
             self.next_state[k] = dist
 
     # observation (APを'__'で連結した文字列) をAPの辞書順で整列させる
-    def __sort_observation(observation) -> Observation:
+    def __sort_observation(observation : str) -> Observation:
         obs_list = sorted(observation.split('__'))
         return "__".join(obs_list)
